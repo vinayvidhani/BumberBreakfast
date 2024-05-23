33fa3489f9a1c7b734a512ab7f5a5c549d6fe145
vinayv-coditas <vinay.vidhani@coditas.com>
2024-05-23 14:33:34 +0530

    Updated Connection string

--- BumberBreakfast/Program.cs
+++ BumberBreakfast/Program.cs
@@ -1,6 +1,9 @@
 using BumberBreakfast.Services.DbServices;
 using BumberBreakfast.Services.RepoServices;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+//builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+//{
+//    var settings = config.Build();
+//    config.AddAzureAppConfiguration(options =>
+//    {
+//        options.Connect("Endpoint=https://myappconfig8492.azconfig.io;Id=xBlI;Secret=kyLxx4Lzk7A7RGe8hKT0KWHEOcbBhI/3RbzSeYbNmU4=");
+//    });
+//});
+
 builder.Services.AddScoped<IBreakfastRepository, BreakfastRepository>();
+//var connectionstring = builder.Configuration.GetSection("Comman:ConnectionString").GetValue<string>("dbpassword");
+
 builder.Services.AddDbContext<BreakfastDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
@@ -23,7 +37,7 @@ if (app.Environment.IsDevelopment())
 //app.UseExceptionHandler("/errors");
 app.UseDeveloperExceptionPage();
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 //app.UseAuthorization();
 
